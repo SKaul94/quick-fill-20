c336c31a9e0987b8f4717b4dbb3cef2bed808e19
SKaul94 <SebastianK94@googlemail.com>
2024-02-02 20:29:32 +0100

    use window.location.origin as default

--- index.js
+++ index.js
@@ -261,7 +261,7 @@ function managePdfList( key, pdfFile ){
  */
 export function cloudLoader( target ){
   return async event => {
-    const url = prompt(`Bitte URL von ${target} eingeben!`, `http://127.0.0.1:5500/${target}`);
+    const url = prompt(`Bitte URL von ${target} eingeben!`, `${window.location.origin}/${target}`);
     if ( url ){
       const fileName = url.split('/').pop();
       const fileNameSuffix = fileName.slice(-4).toLowerCase();
